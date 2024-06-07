0120542dd021a65080153ba8de014856b6cb3082
G Roshini Prasad <groshiniprasad@Roshinis-MacBook-Pro.local>
2024-06-07 19:11:35 +0300

    Cleaned up server code

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-
-// import express, { Request, Response } from "express";
- 
-// const app = express();
-
-// app.get("/", (req: Request, res: Response) => res.json({
-//     staus: "sucess!!!ss"
-// }));
-
-// const port = process.env.PORT || 4000;
-
-// app.listen(port, () => console.log('hey this appliction runs on this port'));
-
 import express, { Application } from "express";
 import Routes from "./routes";
 import Database from "./db";
